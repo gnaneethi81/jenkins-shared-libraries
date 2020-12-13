431afe97d638b7038f2fb24998b98dafb32a7d1c
gnaneethi81 <gnaneethi81@gmail.com>
2020-12-12 19:05:09 -0500

    shared-lib

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,6 +1,6 @@
          
 def call(tomcatIp,tomcatUsr,finalWarName){
-  sh "mv target/myweb*.war target/${finalWarName}.war
+  sh "mv target/myweb*.war target/${finalWarName}.war"
   sshagent(['2e29b303-b5bd-4247-a3ce-ebceb61fb037']){
        sh "scp -o StrictHostKeyChecking=no target/${finalWarName}.war ${tomcatUsr}@${tomcatIp}:/opt/tomcat8/webapps"
         sh """
